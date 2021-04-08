c3951865c42ec0d75ac0c09d1dfa4548fa033e13
Diamantis Sellis <sellisd@gmail.com>
2021-04-08 21:35:41 +0200

    Adapt interface to word, syllable and phoneme level

--- app.R
+++ app.R
@@ -5,6 +5,14 @@ library(shinyFeedback)
 library(readr)
 library(glue)
 library(gitlink)
+library(hyphenatr)
+
+
+construct_delimiter <- " "
+constituent_delimiter <- "-"
+subconstituent_delimiter <- "/"
+discontinued_constituent_delimiter_begin <- "{"
+discontinued_constituent_delimiter_end <- "}"
 
 equation_list = list("MAL"="$$y = ax^be^{-cx}$$",
                      "simplified_1"= "$$y = ae^{-cx}$$",
@@ -18,41 +26,66 @@ error_codes = list("1" = "Discontinued constituent delimiters are not balanced",
                    "2" = "Construct delimiter is not after a constituent and a subconstituent delimiter",
                    "3" = "Constituent delimiter is not after a subconstituent delimiter")
 
+graphemes <- c("p"="h","c"="k","q"="u","c"="h","s"="h","t"="h","n"="g")
+
+merge_characters_list <- function(x){
+    merged <- vector(mode="list", length = length(x))
+    for(i in c(1:length(x))){
+        merged[[i]] <- merge_characters(x[[i]])
+    }
+    merged
+}
+
+#' Merge consecutive characters
+#'
+#' @param x 
+#'
+#' @return array with merged characters
+#' @export
+#'
+#' @examples
+merge_characters <- function(x){
+  #in a character array merge consecutive characters that form a single grapheme
+  if(length(x)<=2){
+      return(x)
+  }
+  to_merge <- numeric(0)
+  previous <- x[1]
+  for(i in c(2:length(x))){
+      if( (x[i] == previous) ||
+          ((previous %in% names(graphemes)) && (graphemes[previous] ==x[i])) ){
+          to_merge <- c(to_merge,i)
+          x[i-1] = paste0(x[i-1],x[i], sep="", collapse = "")
+      }
+      previous = x[i]
+  }
+  if(length(to_merge>0)){
+      x <- x[-to_merge]
+  }
+  return(x)
+}
+
+collapse_words <- function(x){
+    paste(sapply(x,paste,collapse="/"),collapse="/-")
+}
+
 # Define UI for application that draws a histogram
 ui <- fluidPage(
+    titlePanel("Menzerath's law in words, syllables and phonemes"),
     theme = bslib::bs_theme(bootswatch = "pulse"),
     shinyFeedback::useShinyFeedback(),
-    ribbon_css("https://github.com/sellisd/menzerathApp", position = "right", text = "Fork me on GitHub"),
+    ribbon_css("https://github.com/sellisd/menzerathApp/issues", position = "right", text = "Report a bug"),
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
+            p(" annotate it with dashes (-) separating syllables and slashes (/) separating phonemes."),
             fileInput("upload", "Upload a text file to analyze"),
             textAreaInput("text",
                           label = "or type / paste text",
-                          value = "Greece* {which* is* the* most* beau*ti*ful* coun*try* +I* know* +}was* the* first* place* +we* vi*si*ted* in* Eu*ro*pe* +.",
+                          value = "G/r/ee/c/e/- wh/i/ch/- i/s/- th/e/- m/o/s/t/- b/e/au/-t/i/-f/u/l/- c/ou/n/-t/r/y/- I/- k/n/o/w/- w/a/s/- th/e/- f/i/r/s/t/- p/l/a/c/e/- w/e/- v/i/s/-i/t/e/d/- i/n/- E/u/r/o/p/e/.",
                           rows = "10",
-                          resize = "both"),
-            p("Delimiters should be a single character"),
-            textInput("construct_delimiter",
-                      label = "Construct delimiter",
-                      value = "+",
-                      placeholder = "+"),
-            textInput("constituent_delimiter",
-                      label = "Constituent delimiter",
-                      value = " ",
-                      placeholder = " "),
-            textInput("subconstituent_delimiter",
-                      label = "Subconstituent delimiter",
-                      value = "*",
-                      placeholder = "*"),
-            textInput("discontinued_constituent_delimiter_begin",
-                      label = "Discontinued constituent delimiter begin",
-                      value = "{",
-                      placeholder = "{"),
-            textInput("discontinued_constituent_delimiter_end",
-                      label = "Discontinued constituent delimiter end",
-                      value = "}",
-                      placeholder = "}"),
+                          resize = "none"),
+            actionButton("autoannotate", "Auto-annotate"),
             selectInput("method",
                         "Fitting Method",
                         c("MAL",
@@ -82,14 +115,24 @@ ui <- fluidPage(
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
     #thematic::thematic_shiny()
-    
+    observeEvent(input$autoannotate, {
+        words <- strsplit(input$text, split = " ", fixed = TRUE)[[1]]
+        tryCatch({
+          hyphenated <- hyphenate(words, simplify = FALSE)
+          graphemes_proxy <- sapply(hyphenated,strsplit, split="")
+          graphemes_proxy <- sapply(graphemes_proxy, merge_characters_list)
+          updateTextAreaInput(session, "text", value = paste(sapply(graphemes_proxy,collapse_words),collapse="/- "))
+          },
+          error = function(e){showNotification("This is an error.")})
+        
+    })
     mz_object <- reactive({
         counts_df <- process_text(input$text,
-                                  input$construct_delimiter,
-                                  input$constituent_delimiter,
-                                  input$subconstituent_delimiter,
-                                  input$discontinued_constituent_delimiter_begin,
-                                  input$discontinued_constituent_delimiter_end)
+                                  construct_delimiter,
+                                  constituent_delimiter,
+                                  subconstituent_delimiter,
+                                  discontinued_constituent_delimiter_begin,
+                                  discontinued_constituent_delimiter_end)
         is_valid <- !('Errors' %in% colnames(counts_df))
         shinyFeedback::feedbackDanger("text", !is_valid, glue("Error: {error_codes[counts_df$Errors]}"))
         req(is_valid)
@@ -110,11 +153,6 @@ server <- function(input, output, session) {
     })
     output$equation <- renderUI(withMathJax(equation_list[[input$method]]))
     output$parameters <- renderTable({
-        validate_construct_delimiter()
-        validate_constituent_delimiter()
-        validate_subconstituent_delimiter()
-        validate_discontinued_constituent_delimiter_begin()
-        validate_discontinued_constituent_delimiter_end()
         mz <- mz_object()
         if(nobs(mz)==0){
             df <- data.frame()
@@ -128,39 +166,6 @@ server <- function(input, output, session) {
     is_cpp_char <- function(x){
         is.character(x) && nchar(x) == 1 && length(x) == 1
     }
-
-    # User feedback for each delimiter    
-    validate_construct_delimiter <- reactive({
-        is_char <- is_cpp_char(input$construct_delimiter)
-        shinyFeedback::feedbackDanger("construct_delimiter", !is_char, "delimiter must be single character")
-        req(is_char)
-        input$construct_delimiter
-    })
-    validate_constituent_delimiter <- reactive({
-        is_char <- is_cpp_char(input$constituent_delimiter)
-        shinyFeedback::feedbackDanger("constituent_delimiter", !is_char, "delimiter must be single character")
-        req(is_char)
-        input$constituent_delimiter
-    })
-    validate_subconstituent_delimiter <- reactive({
-        is_char <- is_cpp_char(input$subconstituent_delimiter)
-        shinyFeedback::feedbackDanger("subconstituent_delimiter", !is_char, "delimiter must be single character")
-        req(is_char)
-        input$subconstituent_delimiter
-    })
-    validate_discontinued_constituent_delimiter_begin <- reactive({
-        is_char <- is_cpp_char(input$discontinued_constituent_delimiter_begin)
-        shinyFeedback::feedbackDanger("discontinued_constituent_delimiter_begin", !is_char, "delimiter must be single character")
-        req(is_char)
-        input$discontinued_constituent_delimiter_begin
-    })
-    validate_discontinued_constituent_delimiter_end <- reactive({
-        is_char <- is_cpp_char(input$discontinued_constituent_delimiter_end)
-        shinyFeedback::feedbackDanger("discontinued_constituent_delimiter_end", !is_char, "delimiter must be single character")
-        req(is_char)
-        input$discontinued_constituent_delimiter_end
-    })
-    
     # Upload text
     observe({
         req(input$upload)
